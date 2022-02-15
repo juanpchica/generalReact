39bff91aab9e2903c35010931446861621c39bb8
Juan P Chica <juanpchica@hotmail.com>
2022-02-14 21:36:59 -0500

    basic list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 };
